565b8435d770579b6e5ff523f6947b23587abe5b
Etienne Baqué <etiennebaque@gmail.com>
2017-12-10 17:39:23 -0500

    Added email and linkedin

--- js/general.js
+++ js/general.js
@@ -6,20 +6,20 @@ document.addEventListener('DOMContentLoaded', function () {
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-        $el.addEventListener('click', function () {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
 
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
+            // Get the target from the "data-target" attribute
+            var target = $el.dataset.target;
+            var $target = document.getElementById(target);
 
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+            // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+            $el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
 
+            });
         });
-    });
     }
     
 });
